7992a3c4fc4f59ba87680fdde724d4c9d9c682f8
Edilson Silva <edilsonbackend@gmail.com>
2020-01-08 11:07:17 -0300

    Projeto WebService Node

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000;
+
+mongoose = require('./api/models/msgModel'),
+bodyParser = require('body-parser');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/msgdb');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+var routes = requires('./api/routes/msgRoutes');
+routes(app);
+
+
+app.listen(port);
+// app.get('/', function(req, res){
+//     res.json({hello : 'world'});
+// });
+console.log("Message RESTfull API  server stared on: "+port)
