dd2d1010ed63b877ba7824e2b2372a2b04c7ef3f
Edilson Silva <edilsonbackend@gmail.com>
2020-01-08 11:16:46 -0300

    Correçao do server.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000;
 
-mongoose = require('./api/models/msgModel'),
+mongoose = require('mongoose'), 
+Message = require('./api/models/msgModel'),
 bodyParser = require('body-parser');
 mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/msgdb');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 var routes = requires('./api/routes/msgRoutes');
